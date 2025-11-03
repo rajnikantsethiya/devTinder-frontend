30f8d9a9ea44370bd28bbb75864468c3184f9e8f
rajnikantsethiya <rajnikant04sethiya@gmail.com>
2025-11-03 18:06:18 +0530

    Updated hostname endpoint

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "/api";
-// location.hostname === "localhost" ? "http://localhost:3000" : "/api";
+export const BASE_URL =
+    location.hostname === "localhost" ? "http://localhost:3000" : "https://devtinder.iamrsethiya.com";
