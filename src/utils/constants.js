97e37c558518e68661003602a269e2d0c2db57b7
rajnikantsethiya <rajnikant04sethiya@gmail.com>
2025-11-05 14:39:59 +0530

    updated based url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 export const BASE_URL =
-    location.hostname === "localhost" ? "http://localhost:3000" : "https://devtinder.iamrsethiya.com";
+    location.hostname === "localhost" ? "http://localhost:3000" : "https://devtinder.iamrsethiya.com/api";
